3247902c703fce4ad466d8c38dfb005f2501330a
Faisal Al-Darees <faisalaldarees@gmail.com>
2020-06-25 05:07:43 +0300

    Added async task to resend view

--- api/v1/views/email_verification_views.py
+++ api/v1/views/email_verification_views.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 
 from django.utils import timezone
 
-from api.v1.utils import send_verification_email
+from api.v1.tasks import send_verification_email_task
+
 
 from users.models import UserEmailVerification
 
@@ -45,7 +46,7 @@ class ResendEmail(generics.RetrieveAPIView):
         wait_time = timezone.now() - timedelta(seconds=30)
         user_email_verification = self.get_object()
         if user_email_verification.created_at < wait_time:
-            send_verification_email(user_email_verification)
+            send_verification_email_task.delay(user_email_verification.user.email)
             return Response({"resent": True}, status=status.HTTP_200_OK)
         else:
             return Response({"resent": False}, status=status.HTTP_400_BAD_REQUEST)
